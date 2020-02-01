b9a937cc8cdf4c04a92cdb90bb63f27612bffecc
Greg <remph819@gmail.com>
2020-01-31 20:07:25 -0700

    finished protected route and started reducers

--- src/App.js
+++ src/App.js
@@ -1,69 +1,67 @@
 import React from 'react';
-import { Switch, Link, Route } from "react-router-dom"
-
+import { Switch, Link, Route } from 'react-router-dom';
 
 //===============Imports Components=================//
-import LandingPage from "./components/landingpage/LandingPage"
-import Login from "./components/login/Login"
-import UserRegister from "./components/registeration/UserRegister"
-import CompanyRegister from "./components/registeration/CompanyRegister"
-import UserProfile from "./components/profiles/UserProfile"
-import CompanyProfile from "./components/profiles/CompanyProfile"
-import UserEdit from "./components/editprofile/UserEdit"
-import CompanyEdit from "./components/editprofile/CompanyEdit"
-import UserMatch from "./components/matchingpage/UserMatch"
-import CompanyMatch from "./components/matchingpage/CompanyMatch"
+import LandingPage from './components/landingpage/LandingPage';
+import Login from './components/login/Login';
+import UserRegister from './components/registeration/UserRegister';
+import CompanyRegister from './components/registeration/CompanyRegister';
+import UserProfile from './components/profiles/UserProfile';
+import CompanyProfile from './components/profiles/CompanyProfile';
+import UserEdit from './components/editprofile/UserEdit';
+import CompanyEdit from './components/editprofile/CompanyEdit';
+import UserMatch from './components/matchingpage/UserMatch';
+import CompanyMatch from './components/matchingpage/CompanyMatch';
+import ProtectedRoute from './components/ProtectedRoute';
 //==================================================//
 
-
 function App() {
-  return (
-    <div className="App">
+	return (
+		<div className="App">
+			<Switch>
+				<Route exact path="/">
+					<LandingPage />
+				</Route>
 
-      <Switch>
+				<Route path="/login">
+					<Login />
+				</Route>
 
-        <Route exact path="/">
-          <LandingPage />
-        </Route>
+				<Route path="/user_register">
+					<UserRegister />
+				</Route>
 
-        <Route path="/login">
-          <Login />
-        </Route>
+				<Route path="/company_register">
+					<CompanyRegister />
+				</Route>
 
-        <Route path="/user_register">
-          <UserRegister />
-        </Route>
+				<Route path="/user_profile">
+					<UserProfile />
+				</Route>
 
-        <Route path="/company_register">
-          <CompanyRegister />
-        </Route>
+				<Route path="/company_profile">
+					<CompanyProfile />
+				</Route>
 
-        <Route path="/user_profile">
-          <UserProfile />
-        </Route>
+				<Route path="/user_edit">
+					<UserEdit />
+				</Route>
 
-        <Route path="/company_profile">
-          <CompanyProfile />
-        </Route>
+				<Route path="/company_edit">
+					<CompanyEdit />
+				</Route>
 
-        <Route path="/user_edit">
-          <UserEdit />
-        </Route>
+				<Route path="/user_matched">
+					<UserMatch />
+				</Route>
 
-        <Route path="/company_edit">
-          <CompanyEdit />
-        </Route>
-
-        <Route path="/user_matched">
-          <UserMatch />
-        </Route>
-
-        <Route path="/company_matched">
-          <CompanyMatch />
-        </Route>
-      </Switch>
-    </div>
-  );
+				<Route path="/company_matched">
+					<CompanyMatch />
+				</Route>
+			</Switch>
+			<ProtectedRoute exact path="/login" component={UserProfile} />
+		</div>
+	);
 }
 
 export default App;
