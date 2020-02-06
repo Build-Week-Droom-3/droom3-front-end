585996b84c58dd1975a5b60ed56b0c1ba57d1054
hoangvu ong(devin) <hoangvu@hoangvus-MacBook-Air.local>
2020-02-05 16:17:18 -0800

    added css

--- src/components/registeration/UserRegister.js
+++ src/components/registeration/UserRegister.js
@@ -5,38 +5,45 @@ import { Form, Field, withFormik } from "formik"
 import { Link } from "react-router-dom"
 import * as Yup from "yup"
 
-import "./UserRegister.scss"
+import "./Register.scss"
 
-function UserRegister({errors, status }){
+function UserRegister({ errors, status }) {
     const [user, setUser] = useState([]);
 
     useEffect(() => {
         status && setUser(user => [...user, status]);
     }, [status]);
 
-    return(
+    return (
         <div className="user-register-container">
             Employee Register
             <Form className="form-user-register-container">
-                {errors.username && <p>{errors.username}</p>}
-                <Field className="field-user-register" type="text" name="username" placeholder="Username..."/>
-                <Field className="field-user-register"type="text" name="password" placeholder="Password..."/>
-                <Field className="field-user-register"type="text" name="email" placeholder="Email..."/>
+                <Field className="field-user-register" type="text" name="username" placeholder="Username..." />
+
+                <Field className="field-user-register" type="text" name="password" placeholder="Password..." />
+
+                <Field className="field-user-register" type="email" name="email" placeholder="Email..." />
                 <label class="label-terms">
                     <Field className="field-user-submit-terms" type="checkbox" name="terms" />
                     <div className="terms">Terms and Conditions</div>
+                    <div className="already-registered">Already registered? <Link className="sign-in" to="/login">Sign in</Link></div>
+
                 </label>
                 <button className="user-register-btn" type="submit">Register</button>
+                {errors.username && <p>{errors.username}</p>}
+                {errors.password && <p>{errors.password}</p>}
+                {errors.email && <p>{errors.email}</p>}
+                {errors.terms && <p>{errors.terms}</p>}
+
             </Form>
-            <div>Already registered? <Link className="sign-in" to="/login">Sign in</Link></div>
 
             {user.map(ele => (
-            <div>
-                <div>{ele.username}</div>
-                <div>{ele.password}</div>
-                <div>{ele.email}</div>
+                <div>
+                    <div>{ele.username}</div>
+                    <div>{ele.password}</div>
+                    <div>{ele.email}</div>
 
-            </div>
+                </div>
             ))}
             <Link className="back" to="/">{`${"<<"} Back`}</Link>
 
@@ -55,26 +62,27 @@ const FormikUserRegister = withFormik({
     },
     validationSchema: Yup.object().shape({
         username: Yup.string()
-        .min(6)
-        .required("Please enter username."),
+            .min(6)
+            .required("Please enter username."),
         password: Yup.string()
-        .min(6)
-        .required("Please enter password"),
+            .min(6)
+            .required("Please enter password"),
         email: Yup.string()
-        .required("Please enter email")
+            .required("Please enter email"),
+        terms: Yup.bool()
+            .oneOf([true], 'Please accept terms and conditions')
     }),
 
-    handleSubmit(values, {setStatus, resetForm}){
+    handleSubmit(values, { setStatus, resetForm }) {
         console.log(values)
-
         axios
-        .post("https://reqres.in/api/users", values)
-        .then(res => {
-            console.log("Success", res)
-            setStatus(res.data);
-            resetForm()
-        })
-        .catch(err => console.log(err))
+            .post("https://reqres.in/api/users", values)
+            .then(res => {
+                console.log("Success", res)
+                setStatus(res.data);
+                resetForm()
+            })
+            .catch(err => console.log(err))
     }
 })(UserRegister)
 
